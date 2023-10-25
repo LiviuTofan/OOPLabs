b5b94c29bc249fa004af533470369c1246971666
LiviuTofan <liviutofan@mail.ru>
2023-10-25 10:15:27 +0300

    fix labs

--- Lab3/src/FolderReader.java
+++ Lab3/src/FolderReader.java
@@ -1,25 +1,26 @@
-                    import java.io.File;
+import java.io.File;
+//commit
 
-                    public class FolderReader {
-                        public static void listFilesInFolder(String folderPath) {
-                            File folder = new File(folderPath);
+public class FolderReader {
+    public static void listFilesInFolder(String folderPath) {
+        File folder = new File(folderPath);
 
-                            // Check if the specified path exists and is a directory
-                            if (folder.exists() && folder.isDirectory()) {
-                                File[] files = folder.listFiles();
+        // Check if the specified path exists and is a directory
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
 
-                                // List all files in the folder
-                                if (files != null) {
-                                    for (File file : files) {
-                                        if (file.isFile()) {
-                                            System.out.println(file.getName());
-                                        }
-                                    }
-                                } else {
-                                    System.out.println("The folder is empty.");
-                                }
-                            } else {
-                                System.out.println("Invalid folder path or the folder doesn't exist.");
-                            }
-                        }
+            // List all files in the folder
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        System.out.println(file.getName());
                     }
+                }
+            } else {
+                System.out.println("The folder is empty.");
+            }
+        } else {
+            System.out.println("Invalid folder path or the folder doesn't exist.");
+        }
+    }
+}
